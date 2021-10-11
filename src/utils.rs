93e551cc7c1e3c2e7e1013460490154c0b329d13
Sagara Paranagama <sagara89dev@gmail.com>
2021-10-11 13:29:12 +1100

    lesson_03 Add vector module refactor

---
+++ src/utils.rs
@@ -0,0 +1,14 @@
+pub mod color {
+    use crate::math::vec3::Vec3;
+
+    pub type Color = Vec3;
+
+    impl Color {
+        pub fn write_color(&self, output: &mut String) {
+            let ir = (255.999 * self.x()) as i32;
+            let ig = (255.999 * self.y()) as i32;
+            let ib = (255.999 * self.z()) as i32;
+            output.push_str(&format!("{} {} {}\n", ir, ig, ib));
+        }
+    }
+}
